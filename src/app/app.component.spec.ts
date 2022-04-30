16b138df6f39c425314d0af537d8471a372f5ce9
e-svedera <svetlana.trishkina@ericsson.com>
2022-04-30 18:30:08 +0300

    add linting

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,7 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import {
+  RouterTestingModule
+} from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
@@ -30,6 +32,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('twn-test-project app is running!');
+    expect(
+      compiled.querySelector('.content span')?.textContent)
+      .toContain('twn-test-project app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'twn-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
