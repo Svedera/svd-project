3e654982e6f33112fec7ace064a5f2c4e7952d7f
e-svedera <svetlana.trishkina@ericsson.com>
2022-05-01 16:42:38 +0300

    project settings update

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -27,13 +27,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('twn-test-project');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(
-      compiled.querySelector('.content span')?.textContent)
-      .toContain('twn-test-project app is running!');
-  });
 });
