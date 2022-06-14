7f0f59ed67db5829dcc39494597a4734cae5c044
e-svedera <svetlana.trishkina@ericsson.com>
2022-06-14 17:06:59 +0300

    layout fix

--- src/app/core/enums/icons.ts
+++ src/app/core/enums/icons.ts
@@ -1,3 +1,6 @@
 export enum Icon {
-  Menu = 'fa fa-fw fa-plus'
+  Menu = 'fa fa-fw fa-bars',
+  Article = 'fa fa-fw fa-file',
+  ArticleTable = 'fa fa-fw fa-table',
+  GameOfLife = 'fa fa-fw fa-photo'
 }
