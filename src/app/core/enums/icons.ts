c4354b52049086ef942602e0745d95b3db4def1e
svetlana.trishkina@ericsson.com <svetlana.trishkina@ericsson.com>
2022-06-20 14:46:24 +0300

    game of life restyled

--- src/app/core/enums/icons.ts
+++ src/app/core/enums/icons.ts
@@ -2,5 +2,5 @@ export enum Icon {
   Menu = 'fa fa-fw fa-bars',
   Article = 'fa fa-fw fa-file',
   ArticleTable = 'fa fa-fw fa-table',
-  GameOfLife = 'fa fa-fw fa-photo'
+  GameOfLife = 'fa fa-fw fa-chess-board'
 }
