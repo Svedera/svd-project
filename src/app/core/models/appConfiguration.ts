addd6690b89d544ea0bc3c35d760345bc7982076
e-svedera <svetlana.trishkina@ericsson.com>
2022-07-28 20:25:14 +0300

    configurations

--- src/app/core/models/appConfiguration.ts
+++ src/app/core/models/appConfiguration.ts
@@ -1,22 +1,53 @@
 import { LogLevel } from '@enums/logLevel';
 
-export class AppConfiguration {
-  defaultArticleId: string;
-  defaultListLimit: number;
+export interface Configuration {
+  /**
+   * Configuration values set in `index.html`
+   * Boolean values are represented as strings to avoid case sensitivity:
+   * `false`, `False`, `true`, `True` should all be accepted.
+   */
+  [k: string]: string | number;
+}
+
+export class RuntimeConfiguration implements Configuration {
+  baseUrl: string;
   logLevel: LogLevel;
+
+  constructor(
+    baseUrl: string,
+    logLevel: LogLevel) {
+
+    this.baseUrl = baseUrl;
+    this.logLevel = logLevel;
+  }
+}
+
+export class AppConfiguration {
+  runtime: RuntimeConfiguration;
   timeouts: TimeoutConfiguration;
   apiPaths: ApiConfiguration;
+  article: ArticleConfiguration;
+
+  constructor(
+    article: ArticleConfiguration,
+    runtime: RuntimeConfiguration,
+    apiPaths: ApiConfiguration,
+    timeouts: TimeoutConfiguration) {
+    this.article = article;
+    this.runtime = runtime;
+    this.apiPaths = apiPaths;
+    this.timeouts = timeouts;
+  }
+}
+
+export class ArticleConfiguration {
+  defaultArticleId: string;
+  defaultListLimit: number;
 
   constructor(
     defaultArticleId: string,
-    defaultListLimit: number,
-    logLevel: LogLevel,
-    apiPaths: ApiConfiguration,
-    timeouts: TimeoutConfiguration) {
+    defaultListLimit: number) {
     this.defaultArticleId = defaultArticleId;
-    this.logLevel = logLevel;
-    this.apiPaths = apiPaths;
-    this.timeouts = timeouts;
     this.defaultListLimit = defaultListLimit;
   }
 }
@@ -37,7 +68,7 @@ export class ApiConfiguration {
   constructor(
     articleUrl: string,
     listUrl: string) {
-    this.articleUrl = articleUrl,
-      this.listUrl = listUrl
+    this.articleUrl = articleUrl;
+    this.listUrl = listUrl;
   }
 }
